8aa10c89f50c3ab3ebd251b2917fb55df8b77a76
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-30 01:38:26 +0300

    Initial commit: Simple Plugin Manager MVP inspired by Vencord

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@api': path.resolve(__dirname, './src/api'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+            '@plugins': path.resolve(__dirname, './src/plugins'),
+        },
+    },
+    server: {
+        port: 3000,
+    },
+});
